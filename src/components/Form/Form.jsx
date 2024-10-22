6a1907fc72b664770f44c4785d427c04914972a1
ezequiel stom <ezequielstom@gmail.com>
2024-10-22 15:06:52 -0300

    Refactorizar estilos en componentes de formulario, encabezado, galería y héroe: ajustar altura máxima y alineación

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,67 @@
+import { useState, useEffect } from "react";
+import { toast, Toaster } from "sonner";
+import "./Form.css";
+
 export function Form() {
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		if (isMessageSuccess()) {
+			toast("Gracias por tu mensaje, te responderemos a la brevedad.", {
+				type: "success",
+			});
+		}
+	}, []); // Agrega las dependencias aquí
+
+	const isMessageSuccess = () => {
+		return (
+			new URLSearchParams(window.location.search).get("success") ===
+			"true"
+		);
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		const form = event.target;
+		const formData = new FormData(form);
+
+		// Activar loading antes de la promesa
+		setIsLoading(true);
+
+		try {
+			const response = await fetch(form.action, {
+				method: form.method,
+				body: formData,
+			});
+
+			if (!response.ok) {
+				const errorText = await response.text();
+				throw new Error(
+					`Server responded with ${response.status}: ${errorText}`
+				);
+			}
+
+			// Si el mensaje se envía con éxito
+			toast.success("Mensaje enviado con éxito!");
+			form.reset(); // Resetear el formulario
+		} catch (error) {
+			console.error("Error submitting form:", error);
+			toast.error("Ocurrió un error al enviar el mensaje.");
+		} finally {
+			// Asegúrate de desactivar el loading al final
+			setIsLoading(false);
+		}
+	};
+
 	return (
 		<section className="bg-[--secondary]">
+			<Toaster />
+
 			<div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
 				<div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
 					<div className="lg:col-span-2 lg:py-12">
 						<h1>Contacto</h1>
-						<h2 class="font-semibold leading-8 text-3xl">
+						<h2 className="font-semibold leading-8 text-3xl">
 							Te esperamos en nuestros locales, siempre felices de
 							recibirte.
 						</h2>
@@ -36,56 +92,82 @@ export function Form() {
 						</div>
 					</div>
 
-					<div className=" bg-red-50 p-8  lg:col-span-3 lg:p-12">
-						<form action="#" className="space-y-4">
+					<div className=" bg-red-50 p-8  lg:col-span-3 lg:p-12 relative">
+						{isLoading && (
+							<div className="absolute top-0 left-0 right-0 bottom-0 backdrop-blur-md w-full h-full opacity-75 flex justify-center items-center z-50">
+								<span className="loader"></span>
+							</div>
+						)}
+						<form
+							className="space-y-4 "
+							onSubmit={handleSubmit}
+							method="POST"
+							action="https://formsubmit.co/ezequielstom@gmail.com">
 							<div>
-								<label className="sr-only" htmlFor="name">
-									Nombre
-								</label>
 								<input
-									className="w-full  border-gray-200 p-3 text-sm"
-									placeholder="Nombre"
-									type="text"
-									id="name"
+									type="hidden"
+									name="_subject"
+									value="🌮 La Chingada Web | 📩 Nuevo Mensaje!"
 								/>
+								<input
+									type="hidden"
+									name="_captcha"
+									value="false"
+								/>
+
+								<label>
+									Nombre
+									<input
+										type="text"
+										name="name"
+										id="name"
+										className="w-full  border-gray-200 p-3 text-sm"
+										placeholder="Juan Pérez"
+										required
+									/>
+								</label>
 							</div>
 
 							<div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
 								<div>
-									<label className="sr-only" htmlFor="email">
+									<label>
 										Email
+										<input
+											className="w-full  border-gray-200 p-3 text-sm"
+											placeholder="juan_perez@email.com"
+											type="email"
+											name="email"
+											id="email"
+											required
+										/>
 									</label>
-									<input
-										className="w-full  border-gray-200 p-3 text-sm"
-										placeholder="Email"
-										type="email"
-										id="email"
-									/>
 								</div>
 
 								<div>
-									<label className="sr-only" htmlFor="phone">
+									<label>
 										Teléfono
+										<input
+											className="w-full  border-gray-200 p-3 text-sm"
+											placeholder="+598 XXXX XXXX"
+											type="tel"
+											id="phone"
+											name="phone"
+										/>
 									</label>
-									<input
-										className="w-full  border-gray-200 p-3 text-sm"
-										placeholder="Teléfono"
-										type="tel"
-										id="phone"
-									/>
 								</div>
 							</div>
 
 							<div>
-								<label className="sr-only" htmlFor="message">
+								<label>
 									Mensaje
+									<textarea
+										className="w-full max-h-36 border-gray-200 p-3 text-sm"
+										placeholder="Escribe tu mensaje aquí..."
+										rows="8"
+										name="email"
+										id="email"
+										required></textarea>
 								</label>
-
-								<textarea
-									className="w-full max-h-36 border-gray-200 p-3 text-sm"
-									placeholder="Mensaje"
-									rows="8"
-									id="message"></textarea>
 							</div>
 
 							<div className="mt-4">
