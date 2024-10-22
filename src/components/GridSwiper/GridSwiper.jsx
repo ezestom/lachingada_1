7e2c4e24757cfed089064c9b3140510abf171611
ezequiel stom <ezequielstom@gmail.com>
2024-10-22 20:16:46 -0300

    Refactorizar estilos en componentes de formulario, encabezado, galería y héroe: ajustar altura máxima y alineación

--- src/components/GridSwiper/GridSwiper.jsx
+++ src/components/GridSwiper/GridSwiper.jsx
@@ -1,8 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PhotoSwipeLightbox from "photoswipe/lightbox";
 import "photoswipe/style.css";
+import skeleton from "../../icons/skeleton.svg";
 
 export function GridSwiper(props) {
+	const [isLoading, setIsLoading] = useState(true);
 	useEffect(() => {
 		let lightbox = new PhotoSwipeLightbox({
 			gallery: "#" + props.galleryID,
@@ -17,27 +19,38 @@ export function GridSwiper(props) {
 		};
 	}, []);
 
+	useEffect(() => {
+		setIsLoading(false);
+	}, [isLoading]);
+
 	return (
 		<div
 			className="pswp-gallery w-full h-full grid grid-cols-2 md:grid-cols-4 gap-4 "
 			id={props.galleryID}>
-			{props.images.map((image, index) => (
-				<a
-					href={image.largeURL}
-					data-pswp-width={image.width}
-					data-pswp-height={image.height}
-					data-cropped="true"
-					key={props.galleryID + "-" + index}
-					target="_blank"
-					rel="noreferrer"
-					className="object-cover aspect-square hover:scale-105 transition-transform duration-300 ease-in-out">
+			{props.images.map((image, index) =>
+				isLoading ? (
 					<img
-						src={image.thumbnailURL}
-						alt=""
-						className=" w-full h-full max-h-[500px] object-cover object-center transition-transform duration-300 ease-in-out"
+						src={skeleton.src}
+						className="w-full aspect-square h-full object-cover opacity-10 "
 					/>
-				</a>
-			))}
+				) : (
+					<a
+						href={image.largeURL}
+						data-pswp-width={image.width}
+						data-pswp-height={image.height}
+						data-cropped="true"
+						key={props.galleryID + "-" + index}
+						target="_blank"
+						rel="noreferrer"
+						className="object-cover aspect-square hover:scale-105 transition-transform duration-300 ease-in-out">
+						<img
+							src={image.thumbnailURL}
+							alt=""
+							className=" w-full h-full max-h-[500px] object-cover object-center transition-transform duration-300 ease-in-out"
+						/>
+					</a>
+				)
+			)}
 		</div>
 	);
 }
---
+++ src/components/ui/Hero.jsx
@@ -0,0 +1,34 @@
+import { Header } from "./Header.jsx";
+import MySwal from "../MySwal/MySwal.jsx";
+import BackButton from "../BackButton.astro";
+import { useEffect, useState } from "react";
+import "./Hero.css";
+import bg from "../../img/logorosa.png";
+export function Hero() {
+	const [isloading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		setIsLoading(false);
+	}, []);
+
+	return (
+		<section class="h-[100vh] max-w-[100vw]">
+			<Header client:load />
+			<MySwal client:visible />
+			{isloading ? (
+				<img
+					src={bg.src}
+					className="w-full h-full object-cover opacity-10 animate-pulse"
+				/>
+			) : (
+				<div class="parent pt-16 top-0 -z-10 max-w-[100dvw] overflow-hidden">
+					{[1, 2, 3, 4, 5, 6].map((item) => (
+						<div class={`div${item}`} />
+					))}
+				</div>
+			)}
+
+			
+		</section>
+	);
+}
